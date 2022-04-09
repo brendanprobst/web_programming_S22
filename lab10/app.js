5a4656f55725e033ddf485ff4aed8b3d1bf73bf7
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-04-09 17:17:44 -0400

    started lab 10

--- lab10/app.js
+++ lab10/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const configRoutes = require("./routes");
 configRoutes(app);
+app.use(
+	session({
+		name: "AuthCookie",
+		secret: "some secret string!",
+		resave: false,
+		saveUninitialized: true,
+	})
+);
 app.listen(3000, () => {
 	console.log("Server Started");
 	console.log("Your routes will be running on http://localhost:3000");
