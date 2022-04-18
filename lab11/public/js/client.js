cb8c65438e661f1b4e73037ae6e62e3abef2b42c
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-04-17 22:40:06 -0400

    lab 11

--- lab11/public/js/client.js
+++ lab11/public/js/client.js
@@ -1,45 +1,87 @@
 (function ($) {
 	//function to do all the stuff
 
-	function renderShows() {
-		var shows;
-		var showContainer = document.querySelector("#shows-list");
+	var requestConfig = {
+		method: "GET",
+		url: "http://api.tvmaze.com/shows",
+	};
+	$("#shows-list").empty();
+	$("#shows-list").hide();
+
+	$.ajax(requestConfig).then(function (res) {
+		$("#shows-list").show();
+		for (let show of res) {
+			$("#shows-list").append(
+				`<li><a class="show-link" href='${show._links.self.href}'>${show.name}</a></li>`
+			);
+		}
+		$("a.show-link").on("click", function (event) {
+			event.preventDefault();
+			handleLinkClick(event.target.href);
+		});
+	});
+	function handleLinkClick(link) {
+		let showId = link.substring(link.lastIndexOf("/") + 1, link.length);
 		var requestConfig = {
 			method: "GET",
-			url: "http://api.tvmaze.com/shows",
+			url: `http://api.tvmaze.com/shows/${showId}`,
 		};
 		$.ajax(requestConfig).then(function (res) {
-			shows = res;
-			for (let show of shows) {
-				let showEl = createShowListElement(show);
-				showContainer.appendChild(showEl);
+			$("#shows-list").empty();
+			$("#shows-list").hide();
+			$("#show-details").empty();
+			$("#show-details").show();
+			if (res.name.trim() === "" || res.name === null) {
+				res.name = "N/A";
 			}
-		});
+			if (res.image.medium === null) {
+				console.log("image not found");
+				res.image = "/public/img/not_found.jpeg";
+			}
+			if (res.language.trim() === 0) {
+				res.language = "N/A";
+			}
+			if (res.genres.length !== 0) {
+				res.genres = ["N/A"];
+			}
+			if (!res.rating.average) {
+				res.rating.average = "N/A";
+			}
+			if (!res.network) {
+				res.network.name = "N/A";
+			}
 
-		return shows;
-	}
-	function getShowDetails(id) {
-		console.log("in here");
-		console.log(object);
-		console.log(id);
-		alert("wow");
-		return id;
-	}
-	function createShowListElement(show) {
-		let li = document.createElement("li");
-		let link = document.createElement("a");
-		link.innerHTML = show.name;
-		link.href = show._links.self.href;
-		console.log("here");
-		link.click(function (event) {
-			console.log("here");
-			event.preventDefault();
-			getShowDetails(e, show.id);
+			$("#show-details").append(/*HTML*/ `<div>
+            
+            <h1>${res.name}</h1>
+            <img src="${
+							res.image && res.image.medium
+								? res.image.medium
+								: "/public/img/not_found.jpeg"
+						}" />
+                        <h2>Language</h2>
+                        <p>${res.language}</p>
+                        <h2>Genres</h2>
+                        <ul>
+                            ${res.genres.map((genre) => `<li>${genre}</li>`)}
+                        </ul>
+                        <h2>Average Rating</h2>
+                        <p>
+                        ${res.rating.average}
+                        </p>
+                        <h2>Network</h2>
+                        <p>
+                        ${res.network.name}
+                        </p>
+                        <h2>Summary</h2>
+                        <p>
+                       ${res.summary}
+                        </p>
+            </div>`);
 		});
-		console.log("now here");
-		li.appendChild(link);
-		return li;
 	}
-
-	renderShows();
+	$("#search-form").submit(function (event) {
+		event.preventDefault();
+		console.log($("#search-input".val()));
+	});
 })(window.jQuery);
